658e542f5222c5a9f2c8381eb12771b391695c59
HenryFaul <rhfaul@gmail.com>
2023-04-02 11:01:07 +0200

    Initial Commit

---
+++ Got/GotApp.swift
@@ -0,0 +1,17 @@
+//
+//  GotApp.swift
+//  Got
+//
+//  Created by Henry Faul on 2023/04/02.
+//
+
+import SwiftUI
+
+@main
+struct GotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
